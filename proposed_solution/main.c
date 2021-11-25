6dff5c253957e52c1cd57b0837891d07fac99743
Cyril Koenig <cyril.koenig@telecom-paristech.fr>
2021-11-25 17:25:23 +0100

    corrected file input

--- proposed_solution/main.c
+++ proposed_solution/main.c
@@ -2,15 +2,20 @@
 #include "in_out.h"
 #include "dag.h"
 #include "scheduler.h"
+#include <stdlib.h>
 
 #include <time.h>
 
+// Run me from the proposed_solution directory !!
+
 int main(int argc, char *argv[]){
     clock_t begin, end;
-    reader_function(argv[1]);
+    char in_file[30] = "testcases/test0.json", out_file[30] = "out.txt";
+    system("pwd");
+    reader_function(in_file);
     begin = clock();
     scheduler(); // <- fill this function   
     end = clock();
-    printer_function(argv[2], begin, end);
+    printer_function(out_file, begin, end);
     return 0;
 }
